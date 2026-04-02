17896bc65f147c179f420ae314691e345e49bf43
Max Hack <maha9287@colorado.edu>
2026-04-02 15:16:11 -0600

    positive test

--- ProjectSourceCode/test/server.spec.js
+++ ProjectSourceCode/test/server.spec.js
@@ -29,4 +29,29 @@ describe('Server!', () => {
 
 // *********************** TODO: WRITE 2 UNIT TESTCASES **************************
 
+//register:
+// Example Positive Testcase :
+// API: /add_user
+// Input: {id: 5, name: 'John Doe', dob: '2020-02-20'}
+// Expect: res.status == 200 and res.body.message == 'Success'
+// Result: This test case should pass and return a status 200 along with a "Success" message.
+// Explanation: The testcase will call the /add_user API with the following input
+// and expects the API to return a status of 200 along with the "Success" message.
+
+describe('Testing Add User API', () => {
+  it('positive : /register', done => {
+    chai
+      .request(server)
+      .post('/register')
+      .send({username: 'John Doe', password: 'password'})
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+});
+
+
+
+
 // ********************************************************************************

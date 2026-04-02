c36a884817d481b097635192d4d2218af83f30cb
Ethan Cuthrell <ecuthrell@gmail.com>
2026-04-02 15:36:21 -0600

    Fixed rebase issue

--- ProjectSourceCode/test/server.spec.js
+++ ProjectSourceCode/test/server.spec.js
@@ -29,6 +29,33 @@ describe('Server!', () => {
 
 // *********************** TODO: WRITE 2 UNIT TESTCASES **************************
 
+//register:
+// Example Positive Testcase :
+// API: /add_user
+// Input: {id: 5, name: 'John Doe', dob: '2020-02-20'}
+// Expect: res.status == 200 and res.body.message == 'Success'
+// Result: This test case should pass and return a status 200 along with a "Success" message.
+// Explanation: The testcase will call the /add_user API with the following input
+// and expects the API to return a status of 200 along with the "Success" message.
+
+describe('Testing Add User API', () => {
+  it('positive : /register', done => {
+    chai
+      .request(server)
+      .post('/register')
+      .send({username: 'John Doe', password: 'password'})
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+});
+
+
+
+
+// ********************************************************************************
+
 // ********************************************************************************
 //We are checking POST /add_user API by passing the user info in in incorrect manner. This test case should pass and return a status 400 along with a "Usernmane already exists." message.
 
@@ -52,3 +79,4 @@ describe('Testing Register API', () => {
       });
   });
 });
+
